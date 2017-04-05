0c5014d35f1aa7c6d524b2e9c97032318df268c9
Vitor Horta <vitor.horta@gmail.com>
2017-04-05 20:33:51 -0300

    direcionamento

--- Level.js
+++ Level.js
@@ -1,5 +1,6 @@
 function Level() {
   this.sprites = [];
+  this.shots = [];
   this.inimigos = 3;
 }
 
@@ -10,6 +11,7 @@ Level.prototype.init = function () {
     inimigo.y = 10;
     inimigo.width = 10+i*5;
     inimigo.height = 10+i*5;
+    inimigo.am = 20;
     this.sprites.push(inimigo);
   }
 };
@@ -18,11 +20,42 @@ Level.prototype.mover = function (dt) {
   for (var i = 0; i < this.sprites.length; i++) {
     this.sprites[i].mover(dt);
   }
+  for (var i = this.shots.length-1; i >= 0; i--) {
+    this.shots[i].moverAng(dt);
+    if(
+        this.shots[i].x >  3000 ||
+        this.shots[i].y < -3000 ||
+        this.shots[i].y >  3000 ||
+        this.shots[i].x < -3000
+    ){
+      this.shots.splice(i,i);
+    }
+  }
+}
+
+Level.prototype.moverAng = function (dt) {
+  for (var i = 0; i < this.sprites.length; i++) {
+    this.sprites[i].moverAng(dt);
+  }
+  for (var i = this.shots.length; i >= 0; i--) {
+    this.shots[i].moverAng(dt);
+    if(
+        this.shots[i].x >  3000 ||
+        this.shots[i].y < -3000 ||
+        this.shots[i].y >  3000 ||
+        this.shots[i].x < -3000
+    ){
+      this.shots.splice(i,i);
+    }
+  }
 }
 
 Level.prototype.desenhar = function (ctx) {
   for (var i = 0; i < this.sprites.length; i++) {
     this.sprites[i].desenhar(ctx);
+  }
+  for (var i = 0; i < this.shots.length; i++) {
+    this.shots[i].desenhar(ctx);
   }
 };
 
@@ -37,5 +70,35 @@ Level.prototype.colidiu = function (alvo, resolveColisao) {
 Level.prototype.perseguir = function (alvo, dt) {
   for (var i = 0; i < this.sprites.length; i++) {
     this.sprites[i].perseguir(alvo,dt);
+  }
+}
+
+
+Level.prototype.perseguirAng = function (alvo, dt) {
+  for (var i = 0; i < this.sprites.length; i++) {
+    this.sprites[i].perseguirAng(alvo,dt);
+  }
+}
+
+Level.prototype.fire = function(alvo){
+  if(alvo.cooldown > 0) return;
+  var tiro = new Sprite();
+  tiro.x = alvo.x;
+  tiro.y = alvo.y;
+  tiro.angle = alvo.angle;
+  tiro.am = 100;
+  this.width = 5;
+  tiro.height = 5;
+  this.shots.push(tiro);
+  alvo.cooldown = 1;
+}
+
+Level.prototype.colidiuComTiros = function() {
+  var that = this;
+  for(var i = this.shots.length-1; i>=0; i--) {
+    this.colidiu(this.shots[i], function(alvo){
+      alvo.color = "green";
+      that.shots.splice(i,i);
+    })
   }
 }
--- Sprite.js
+++ Sprite.js
@@ -5,6 +5,7 @@ function Sprite(){
   this.vy = 0;
   this.ax = 0;
   this.ay = 0;
+  this.am = 0;
   this.width = 15;
   this.height = 15;
   this.angle = 0;
@@ -15,11 +16,18 @@ function Sprite(){
 Sprite.prototype.desenhar = function (ctx) {
   ctx.save();
   ctx.translate(this.x,this.y);
-  ctx.rotate(this.angle*Math.PI/180);
+  ctx.rotate(this.angle*2*Math.PI/360);
   ctx.fillStyle = this.color;
-  ctx.fillRect(-this.width/2,-this.height/2,this.width,this.height);
+  ctx.beginPath();
+  ctx.moveTo(-this.width/2, -this.height/2);
+  ctx.lineTo(-this.width/2, +this.height/2);
+  ctx.lineTo(+this.width/2, 0);
+  ctx.closePath();
+  ctx.fill();
   ctx.strokeStyle = "black";
-  ctx.strokeRect(-this.width/2,-this.height/2,this.width,this.width);
+  ctx.stroke();
+  //ctx.strokeStyle = "black";
+  //ctx.strokeRect(-this.width/2,-this.height/2,this.width,this.width);
   ctx.restore();
 };
 
@@ -29,6 +37,19 @@ Sprite.prototype.mover = function (dt) {
   this.x = this.x + (this.vx*dt);
   this.y = this.y + (this.vy*dt);
   this.angle = this.angle + this.vang*dt;
+};
+
+Sprite.prototype.moverAng = function (dt) {
+  this.angle = this.angle + this.vang*dt;
+
+  this.ax = this.am*Math.cos(Math.PI*this.angle/180);
+  this.ay = this.am*Math.sin(Math.PI*this.angle/180);
+
+  this.vx = this.vx + (this.ax)*dt;
+  this.vy = this.vy + (this.ay)*dt;
+  this.x = this.x + (this.vx*dt);
+  this.y = this.y + (this.vy*dt);
+  if(this.cooldown > 0) this.cooldown -= dt;
 };
 
 Sprite.prototype.colidiuCom = function (alvo) {
@@ -43,4 +64,12 @@ Sprite.prototype.colidiuCom = function (alvo) {
 Sprite.prototype.perseguir = function (alvo, dt) {
   this.ax = 5*(alvo.x - this.x)*dt - this.vx*0.1;
   this.ay = 5*(alvo.y - this.y)*dt - this.vy*0.1;
+}
+
+Sprite.prototype.perseguirAng = function (alvo, dt) {
+  var dX = alvo.x - this.x;
+  var dY = alvo.y - this.y;
+  var dist = Math.sqrt(dX*dX+dY*dY);
+  var dA = 180*Math.acos(dX/dist)/Math.PI;
+  this.vang = 100*(dA - this.angle)*dt;
 }
