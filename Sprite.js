f3783f2a85134fe4057df81c4b90da595ec994ff
Vitor Horta <vitor.horta@gmail.com>
2017-04-11 10:33:52 -0300

    primeira fase

--- Sprite.js
+++ Sprite.js
@@ -5,14 +5,19 @@ function Sprite(){
   this.vy = 0;
   this.ax = 0;
   this.ay = 0;
+  this.am = 0;
+  this.width = 15;
+  this.height = 15;
+  this.angle = 0;
+  this.vang = 0;
   this.color  = "blue";
 }
 
 Sprite.prototype.desenhar = function (ctx) {
   ctx.fillStyle = this.color;
-  ctx.fillRect(this.x,this.y,15,15);
+  ctx.fillRect(this.x,this.y,this.width,this.height);
   ctx.strokeStyle = "black";
-  ctx.strokeRect(this.x,this.y,15,15);
+  ctx.strokeRect(this.x,this.y,this.width,this.width);
 };
 
 Sprite.prototype.mover = function (dt) {
@@ -20,4 +25,13 @@ Sprite.prototype.mover = function (dt) {
   this.vy = this.vy + (this.ay+60)*dt;
   this.x = this.x + (this.vx*dt);
   this.y = this.y + (this.vy*dt);
+};
+
+Sprite.prototype.colidiuCom = function (alvo) {
+  if(this.x+this.width < alvo.x) return false;
+  if(this.x > alvo.x+alvo.width) return false;
+  if(this.y+this.width < alvo.y) return false;
+  if(this.y > alvo.y+alvo.height) return false;
+
+  return true;
 };
