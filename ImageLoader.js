5d5e978c408be70867f95bea30474c96f5f900f5
Vitor Horta <vitor.horta@gmail.com>
2017-04-12 20:19:52 -0300

    Add image loader

--- ImageLoader.js
+++ ImageLoader.js
@@ -6,5 +6,8 @@ ImageLoader.prototype.load = function(key, imgUrl) {
   var img = new Image();
   img.src = imgUrl;
   this.images[key] = img;
-
 };
+
+ImageLoader.prototype.drawSprite = function(ctx, key, sx, sy, sw, sh, dx, dy, dw, dh){
+  ctx.drawImage(this.images[key], sx,sy,sw,sh,dx,dy,dw,dh);
+}
