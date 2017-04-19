fc07d99b184a1e77b7d39fe396ee0f2cdebfce38
Vitor Horta <vitor.horta@gmail.com>
2017-04-19 19:53:56 -0300

    ADd audioloader

---
+++ AudioLoader.js
@@ -0,0 +1,29 @@
+function AudioLoader(){
+  this.audios = {};
+  this.canais = [];
+
+  for(var i=0; i < 10; i++){
+    this.canais[i] = {
+      audio: new Audio(),
+      fim: -1
+    }
+  }
+}
+
+AudioLoader.prototype.load = function(key, url) {
+  this.audios[key] = new Audio();
+  this.audios[key].src = url;
+  this.audios[key].load();
+};
+
+AudioLoader.prototype.play = function(key) {
+  var agora = new Date();
+  for(var i=0; i < this.canais.length; i++){
+    var canal = this.canais[i];
+    if(canal.fim < agora.getTime()) {
+      canal.audio.src = this.audios[key].src;
+      canal.fim = agora.getTime() + this.audios[key].duration * 1000;
+      canal.audio.play();
+    }
+  }
+}
